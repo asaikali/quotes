a5d621cc4f6ba3e393a7c189e36edae2b2788fa2
Adib Saikali <asaikali@gmail.com>
2024-01-14 13:09:20 -0500

    Add a vuejs front end

--- microservice-quotes-max-spring-cloud/api-gateway/src/main/java/com/example/GatewayApplication.java
+++ microservice-quotes-max-spring-cloud/api-gateway/src/main/java/com/example/GatewayApplication.java
@@ -2,9 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 // @EnableDiscoveryClient no longer required keeping this comment to point that it is no longer
 // needed
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
