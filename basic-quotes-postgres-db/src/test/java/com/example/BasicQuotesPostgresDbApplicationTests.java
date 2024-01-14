e81b4e6d51eebc2dc6118e35727305394eca889e
Adib Saikali <asaikali@gmail.com>
2024-01-14 03:26:29 -0500

    add test container support

---
+++ basic-quotes-postgres-db/src/test/java/com/example/BasicQuotesPostgresDbApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BasicQuotesPostgresDbApplicationTests {
+
+    @Container
+    @ServiceConnection
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>("postgres:15");
+
+    @Test
+    void contextLoads() {
+    }
+}
