37e27f58d7a1c167d4d2cfea83b42dd67fa2b3f1
Adib Saikali <asaikali@gmail.com>
2024-07-07 20:51:42 +0200

    Polish

--- spa-api-db/quotes-ui/src/App.vue
+++ spa-api-db/quotes-ui/src/App.vue
@@ -1,20 +1,19 @@
 <template>
-    <Billboard />
+  <Billboard />
 </template>
 
 <script>
 // Import the Billboard component
-import Billboard from './components/Billboard.vue';
+import Billboard from './components/BillboardComponent.vue'
 
 export default {
   name: 'App',
   components: {
     Billboard // Register the Billboard component
   }
-};
+}
 </script>
 
 <style>
 /* Add your global styles here */
-
 </style>
